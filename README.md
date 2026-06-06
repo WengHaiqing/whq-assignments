02bbda46157cecf24393083d8cf5d829890290d0
WengHaiqing <2166076494@qq.com>
2026-06-06 11:35:47 +0800

    版本2：修改hello.c增加键盘输入功能，更新README说明

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # whq-assignments
 课程作业仓库
-ex01：第一版HelloWorld程序，仅控制台输出文字
+ex01：
+1. v1：基础HelloWorld，控制台输出
+2. v2：新增scanf键盘输入，读取用户姓名并输出
--- ex01/hello.c
+++ ex01/hello.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 int main()
 {
-    printf("Hello World!\n");
+    char name[50];
+    printf("请输入你的姓名：");
+    scanf("%s", name); //scanf键盘输入函数
+    printf("Hello,%s!\n", name);
     return 0;
 }
